279c9069cd40af7309e9a10ad70395cc1910a6a9
Saparbekov Nurdan <113797353+OVERDOZZZE@users.noreply.github.com>
2023-12-18 06:52:03 +0600

    Update settings.py

--- Quilzet/settings.py
+++ Quilzet/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 CORS_ORIGIN_ALLOW_ALL = True
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
